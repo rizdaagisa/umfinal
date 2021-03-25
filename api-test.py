939f1d0896b589dda0b6d87042f34e3cd7db5bc6
unknown <rizdaagisa@gmail.com>
2021-03-25 13:24:04 +0700

    botum

--- api-test.py
+++ api-test.py
@@ -5,7 +5,6 @@ import secrets
 import pandas as pd
 from datetime import datetime
 import re
-from data import npm,nama,kelas,token_user,nama_matkul,token_matkul,key
 
 
 # generated_key = secrets.token_urlsafe(16)
@@ -21,46 +20,37 @@ from data import npm,nama,kelas,token_user,nama_matkul,token_matkul,key
 
 
 base = "http://127.0.0.1:5000/"
-# base = "https://bot-um.herokuapp.com/"
+base = "https://bot-um.herokuapp.com/"
 
-data = {
-    "npm_user" : "14117220",
-    "admin_token" : "PHBzPlMI2JdWVyrRzdbZg",
-    "login_token" : "",
-    "soal" : '"Do you know what she is going ?" He asked me if .......',
-    "kode" : "KD021216"
-}
+nama = "Muhammad Rizdalah Agisa"
+npm = "14117220"
+kelas = "4ka13"
+key = "56212152172004422125" # Key UM yang ada di studentsite
+token_user = "PHBzPlMI2JdWVyrRzdbZg" #token yang diberikan dari owner
 
-data2= {
-    "nama": nama,
-    "kelas" : kelas,
-    "npm" : npm
-}
+nama_matkul = "Pendidikan Pancasila"  #nama matkul yang akan di kerjakan
+token_matkul = "" #token matkul yang ada di daftar matkul
 
-data3={
-    "npm_admin": "14117220",
-    "admin_token" : "PHBzPlMI2JdWVyrRzdbZg",
-    "npm_user" : "14117220",
-    "nama_user" : "Muhammad Rizdalah Agisa",
-    "kelas_user" : "4ka13",
-    "kode_matkul" : "KK011321",
-    "matkul" : "SISTEM IMPLEMENTASI"
-}
-data = {
-    "npm" : "14117220",
-    "token_user" : "PHBzPlMI2JdWVyrRzdbZg",
-    "soal" : '"Do you know what she is going ?" He asked me if .......',
-    "token_matkul" : "QZflwT0ef0DBlHv1tuGKTw",
-    "path" : "D:/rizda"
-}
+def kunci():
+    data = {
+        "npm" : "14117220",
+        "token_user" : "PHBzPlMI2JdWVyrRzdbZg",
+        "soal" : '"Do you know what she is going ?" He asked me if .......',
+        "token_matkul" : "QZflwT0ef0DBlHv1tuGKTw",
+        "path" : "D:/rizda"
+    }
+    result = requests.post(base+"kunci",json=data)
+    print(result.json())
 
-# result = requests.post(base+"kunci",json=data)
-data = {"npm" : npm, "nama":nama, "kelas": kelas, "token_user" : token_user,"nama_matkul":nama_matkul}
-result = requests.post(base+"login",json=data)
-# data['login_token'] = "6MfR5pkGvHLp09YhTnA3lg"
-# print(data)
-# result = requests.post(base+"/kunci/",data=data)
-# print(data2)
-# result = requests.post(base+"/create_user/",data=data2)
 
-print(result.json())
+def login():
+    data = {"npm" : npm, "nama":nama, "kelas": kelas, "token_user" : token_user,"nama_matkul":nama_matkul}
+    result = requests.post(base+"login",json=data)
+    print(result.json())
+
+def create():
+    data = {"npm" : npm, "nama":nama, "kelas": kelas}
+    result = requests.post(base+"create_user",json=data)
+    print(result.json())
+
+create()
